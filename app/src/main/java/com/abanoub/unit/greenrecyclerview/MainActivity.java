896bd9b0c9de8c68b8fee8e8945475415368cc6e
abanoub ghebreal <abanoub.eskander15@gmail.com>
2018-08-15 16:11:23 +0200

    Step NO.2: Create custom adapter and view holder

--- app/src/main/java/com/abanoub/unit/greenrecyclerview/MainActivity.java
+++ app/src/main/java/com/abanoub/unit/greenrecyclerview/MainActivity.java
@@ -2,12 +2,48 @@ package com.abanoub.unit.greenrecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NUM_LIST_ITEMS = 150;
+
+    private RecyclerView mNumbersList;
+
+    private NumbersAdapter mAdaapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+         * Using findViewById, we get a reference to our RecyclerView from xml. This allows us to
+         * do things like set the adapter of the RecyclerView and toggle the visibility.
+         */
+        mNumbersList = findViewById(R.id.rv_numbers);
+
+        /*
+         * A LinearLayoutManager is responsible for measuring and positioning item views within a
+         * RecyclerView into a linear list. This means that it can produce either a horizontal or
+         * vertical list depending on which parameter you pass in to the LinearLayoutManager
+         * constructor. By default, if you don't specify an orientation, you get a vertical list.
+         * In our case, we want a vertical list.
+         */
+        LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mNumbersList.setLayoutManager(manager);
+
+        /*
+         * Use this setting to improve performance if you know that changes in content do not
+         * change the child layout size in the RecyclerView
+         */
+        mNumbersList.setHasFixedSize(true);
+
+        /*
+         * The NumbersAdapter is responsible for displaying each item in the list.
+         */
+        mAdaapter = new NumbersAdapter(NUM_LIST_ITEMS);
+        mNumbersList.setAdapter(mAdaapter);
     }
 }
--- app/src/main/java/com/abanoub/unit/greenrecyclerview/NumbersAdapter.java
+++ app/src/main/java/com/abanoub/unit/greenrecyclerview/NumbersAdapter.java
@@ -1,35 +1,93 @@
 package com.abanoub.unit.greenrecyclerview;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class NumbersAdapter extends RecyclerView.Adapter<NumbersAdapter.NumberViewHolder>{
 
+    private int mNumberItems;
 
+    /**
+     * Constructor for NumbersAdapter that accepts a number of items to display and the specification
+     * for the ListItemClickListener.
+     *
+     * @param numberItems Number of items to display in list
+     */
+    public NumbersAdapter(int numberItems){
+        this.mNumberItems = numberItems;
+    }
+
+    /**
+     * This gets called when each new ViewHolder is created. This happens when the RecyclerView
+     * is laid out. Enough ViewHolders will be created to fill the screen and allow for scrolling.
+     *
+     * @param parent The ViewGroup that these ViewHolders are contained within.
+     * @param viewType  If your RecyclerView has more than one type of item (which ours doesn't) you
+     *                  can use this viewType integer to provide a different layout. See
+     *                  {@link android.support.v7.widget.RecyclerView.Adapter#getItemViewType(int)}
+     *                  for more details.
+     * @return A new NumberViewHolder that holds the View for each list item
+     */
     @NonNull
     @Override
     public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Context context = parent.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+        return new NumberViewHolder(view);
     }
 
+    /**
+     * OnBindViewHolder is called by the RecyclerView to display the data at the specified
+     * position. In this method, we update the contents of the ViewHolder to display the correct
+     * indices in the list for this particular position, using the "position" argument that is conveniently
+     * passed into us.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(@NonNull NumberViewHolder holder, int position) {
-
+        holder.numberListItem.setText(String.valueOf(position));
     }
 
+    /**
+     * This method simply returns the number of items to display. It is used behind the scenes
+     * to help layout our Views and for animations.
+     *
+     * @return The number of items available in our forecast
+     */
     @Override
     public int getItemCount() {
-        return 0;
+        return mNumberItems;
     }
 
 
+    /**
+     * Cache of the children views for a list item.
+     */
     public class NumberViewHolder extends RecyclerView.ViewHolder{
 
+        // Will display the position in the list, ie 0 through getItemCount() - 1
+        TextView numberListItem;
+
+        /**
+         * Constructor for our ViewHolder. Within this constructor, we get a reference to our
+         * TextViews and set an onClickListener to listen for clicks. Those will be handled in the
+         * onClick method below.
+         * @param itemView The View that you inflated in
+         * {@link NumbersAdapter#onCreateViewHolder(ViewGroup, int)}
+         */
         public NumberViewHolder(View itemView) {
             super(itemView);
+
+            numberListItem = itemView.findViewById(R.id.tv_number);
         }
     }
 }
